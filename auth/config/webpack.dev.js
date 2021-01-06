ec02c3ee9c0c3f00471c1e3064ff49d4a5c04933
Mike Iden <michaeliden90@gmail.com>
2021-01-05 20:51:39 -0500

    Adding auth

--- auth/config/webpack.dev.js
+++ auth/config/webpack.dev.js
@@ -1,31 +1,33 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
-const packageJson = require("../package.json");
 const commonConfig = require('./webpack.common');
-const port = 8080;
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: `http://localhost:8082/`
+    publicPath: 'http://localhost:8082/',
   },
   devServer: {
     port: 8082,
     historyApiFallback: {
-      index: "index.html"
+      index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: 'auth',
       filename: 'remoteEntry.js',
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js"
+      exposes: {
+        './AuthApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies
-    })
-  ]
-}
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  onNavigate && history.listen(onNavigate);
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
   );
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const devElement = document.querySelector('#dev-marketing');
-  devElement && mount(devElement);
+  devElement && mount(devElement, { defaultHistory: createBrowserHistory() });
 }
 
 export { mount };
